1e7fce14948db5ce582e08674dc46c4d790d00ef
Garrett <garrettpyke@msn.com>
2022-03-30 00:26:21 -0600

    Add PATCH method to shopping_lists view, tests OK

--- api/views/shopping_lists.py
+++ api/views/shopping_lists.py
@@ -38,6 +38,17 @@ class ShoppingListItemView(APIView):
         else:
             list_item.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-                       
 
+    def patch(self, request, pk):
+        list_item = get_object_or_404(ShoppingList, pk=pk)
+        if request.user != list_item.shopper_id:
+            raise PermissionDenied('Unauthorized, this item belongs to another shopper')
+        else:
+            request.data['shopper_id'] = request.user.id
+            updated_list_item = ShoppingListSerializer(list_item, data=request.data, partial=True)
+            if updated_list_item.is_valid():
+                updated_list_item.save()
+                return Response(updated_list_item.data)
+            else:
+                return Response(updated_item.errors, status=status.HTTP_400_BAD_REQUEST)
         
